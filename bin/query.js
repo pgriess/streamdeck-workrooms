b587885198693a15438cb300a4c4a210af03a46a
Peter Griess <peter.griess@oculus.com>
2021-06-25 10:00:00 -0500

    More pervasive use of regexes
    - Use regular expressions to match expected button values and map them
      to states.
    - Unify regular expressions to identify buttons between toggle.js and
      query.js
    
    Fixes #19

--- bin/query.js
+++ bin/query.js
@@ -3,31 +3,45 @@
 // of values rather than a single one so that we can avoid issuing multiple
 // queries per tick.
 () => {
+    // Regular expressions to idetify buttons. These must be kept in-sync
+    // between toggle.js <=> query.js
+    // ***** BEGIN *****
+    let micButtonRegex = /microphone/i;
+    let cameraButtonRegex = /video|camera/i;
+    let handButtonRegex = /hand/i;
+    // ***** END *****
+
+    let micOffRegex = /^unmute microphone$/i;
+    let micOnRegex = /^mute microphone$/i;
     let micText = Array.from(document.querySelectorAll('button'))
-        .filter((n) => { return n.textContent.match(/microphone/i); })
+        .filter((n) => { return n.textContent.match(micButtonRegex); })
         .map((n) => { return n.textContent; })
         .join('');
     let micState =
-        (micText === "Unmute microphone") ? "OFF" :
-        (micText === "Mute microphone") ? "ON" :
+        (micText.match(micOffRegex)) ? "OFF" :
+        (micText.match(micOnRegex)) ? "ON" :
         "UNKNOWN";
 
+    let cameraOffRegex = /^turn on (camera|video)$/i;
+    let cameraOnRegex = /^turn off (camera|video)$/i;
     let cameraText = Array.from(document.querySelectorAll('button'))
-        .filter((n) => { return n.textContent.match(/video/i); })
+        .filter((n) => { return n.textContent.match(cameraButtonRegex); })
         .map((n) => { return n.textContent; })
         .join('');
     let cameraState =
-        (cameraText === "Turn on video") ? "OFF" :
-        (cameraText === "Turn off video") ? "ON" :
+        (cameraText.match(cameraOffRegex)) ? "OFF" :
+        (cameraText.match(cameraOnRegex)) ? "ON" :
         "UNKNOWN";
 
+    let handOffRegex = /^raise hand$/i;
+    let handOnRegex = /^lower hand$/i;
     let handText = Array.from(document.querySelectorAll('button'))
-        .filter((n) => { return n.textContent.match(/hand/i); })
+        .filter((n) => { return n.textContent.match(handButtonRegex); })
         .map((n) => { return n.textContent; })
         .join('');
     let handState =
-        (handText === "Raise hand") ? "OFF" :
-        (handText === "Lower hand") ? "ON" :
+        (handText.match(handOffRegex)) ? "OFF" :
+        (handText.match(handOnRegex)) ? "ON" :
         "UNKNOWN";
 
     return [micState, cameraState, handState].join(" ");
--- bin/toggle.js
+++ bin/toggle.js
@@ -1,8 +1,16 @@
 (target) => {
+    // Regular expressions to idetify buttons. These must be kept in-sync
+    // between toggle.js <=> query.js
+    // ***** BEGIN *****
+    let micButtonRegex = /microphone/i;
+    let cameraButtonRegex = /video|camera/i;
+    let handButtonRegex = /hand/i;
+    // ***** END *****
+
     let re =
-        (target === "mic") ? /microphone/i :
-        (target === "camera") ? /video/i :
-        (target === "hand") ? /hand/i :
+        (target === "mic") ? micButtonRegex :
+        (target === "camera") ? cameraButtonRegex :
+        (target === "hand") ? handButtonRegex :
         null;
 
     if (re === null) {
