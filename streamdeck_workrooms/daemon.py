a1e53c15e9e7c87aa83b340331d5e0b5d88d2aaa
Peter Griess <peter.griess@oculus.com>
2021-08-30 20:55:27 -0500

    Move browser_listen() to browser module

--- streamdeck_workrooms/daemon.py
+++ streamdeck_workrooms/daemon.py
@@ -1,4 +1,5 @@
 from . import analytics
+from . import browser
 from . import streamdeck
 from .types import ActionState
 
@@ -17,16 +18,6 @@ import time
 import traceback
 from uuid import UUID
 import websockets
-
-
-# Grace period before we show the user an error
-ERROR_GRACE_PERIOD_SECONDS = 5
-
-
-# User-facing error codes
-EC_QUERY_SUBPROCESS_FAILED = 'E1'
-EC_QUERY_DOM_FAILED = 'E2'
-EC_CHROME_APPLESCRIPT_DISABLED = 'E3'
 
 
 # Global state for each action
@@ -60,157 +51,6 @@ action_metadata = {
         'next_time': 0,
     },
 }
-
-
-# Task to get state updates from the browser
-async def browser_listen(ws, analytics_collect, on_images, off_images, unknown_images, none_images):
-    MIC_INDEX = action_metadata['mic']['index']
-    CAMERA_INDEX = action_metadata['camera']['index']
-    HAND_INDEX = action_metadata['hand']['index']
-    CALL_INDEX = action_metadata['call']['index']
-
-    while True:
-        await asyncio.sleep(1)
-
-        now = time.time()
-        status_array = [None] * len(action_metadata)
-        errors_array = [None] * len(action_metadata)
-
-        # Fill in the *_array values by querying browser
-        #
-        # Error handling here is quite involved, as there are many different
-        # layers in which things could fail. Some of these errors mean that we
-        # can't interpret any results (e.g. failed to execute the query script),
-        # while some are partial (e.g. we can't find the "hand" button).
-        try:
-            proc = await asyncio.create_subprocess_exec(
-                os.path.join(os.path.curdir, 'query_browser_state.osa'),
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE)
-
-            out, err = await proc.communicate()
-            out = out.decode('utf-8').strip()
-            err = err.decode('utf-8').strip()
-
-            if proc.returncode == 0:
-                # The 'NONE' sentinel value means no rooms were found. Expand that
-                # to fill each of the actions rather than doing that in query.js
-                if out == 'NONE':
-                    status_array = ['NONE'] * len(action_metadata)
-                else:
-                    status_array = out.split(' ')
-
-                assert len(status_array) == len(action_metadata)
-
-                # Some calls don't have a hand status, which will cause it to
-                # come back from the query as UNKNOWN. In this case, don't show
-                # the user the confusing UNKNOWN icon. Just consider it NONE
-                # since this is expected.
-                if status_array[HAND_INDEX] == 'UNKNOWN' and \
-                        status_array[MIC_INDEX] in ['ON', 'OFF'] and \
-                        status_array[CAMERA_INDEX] in ['ON', 'OFF'] and \
-                        status_array[CALL_INDEX] in ['ON', 'OFF']:
-                    status_array[HAND_INDEX] = 'NONE'
-
-                # Any entries in the status_array that are UNKNOWN mean
-                # that we couldn't find the DOM node. Mark this as an error in
-                # the relevant field.
-                #
-                # XXX: This isn't really true as we could have found the node but
-                #      not been able to interpret its contents. Update query.js to
-                #      differentiate the different failure modes.
-                for index, status in enumerate(status_array):
-                    if status != 'UNKNOWN':
-                        continue
-
-                    errors_array[index] = EC_QUERY_DOM_FAILED
-            else:
-                error('query failed with status {}\nstdout={}\nstderr={}'.format(proc.returncode, out, err))
-
-                # Compute the error code, defaulting to the generic
-                # EC_QUERY_SUBPROCESS_FAILED
-                ec = EC_QUERY_SUBPROCESS_FAILED
-                if 'Executing JavaScript through AppleScript is turned off' in err:
-                    ec = EC_CHROME_APPLESCRIPT_DISABLED
-
-                status_array = ['UNKNOWN'] * len(action_metadata)
-                errors_array = [ec] * len(action_metadata)
-
-        except Exception:
-            error(traceback.format_exc())
-            status_array = ['UNKNOWN'] * len(action_metadata)
-            errors_array = [EC_QUERY_SUBPROCESS_FAILED] * len(action_metadata)
-
-        # Report query timing, sampled at 1%
-        if randint(0, 100) == 0:
-            await analytics_collect(
-                t='timing',
-                utc='query',
-                utv='subprocess',
-                utt=int((time.time() - now) * 1000))
-
-        assert len(status_array) == len(action_metadata)
-        assert len(errors_array) == len(action_metadata)
-
-        # Loop over all actions and update the Stream Deck accordingly
-        for name, data in action_metadata.items():
-            index = data['index']
-            context = data['context']
-            prev_state = data['current']
-            current_state = data['current']
-            next_state = data['next']
-            next_time = data['next_time']
-
-            new_state = ActionState(status_array[index], errors_array[index])
-
-            # We have no context; don't update
-            if context is None:
-                continue
-
-            # Update the next state if it's changed
-            if new_state != next_state:
-                data['next'] = next_state = new_state
-                data['next_time'] = next_time = now
-
-            # Nothing has changed, no more work to do
-            if next_state == current_state:
-                continue
-
-            # All changes are flushed after ERROR_GRACE_PERIOD_SECONDS
-            if now > (next_time + ERROR_GRACE_PERIOD_SECONDS):
-                data['current'] = current_state = next_state
-
-            # Changes to a "good" status cause the entire next state to be
-            # propagated immediately since we want to be in an error state for
-            # as short a time as possible.
-            if next_state.status != current_state.status and next_state.status != 'UNKNOWN':
-                data['current'] = current_state = next_state
-
-            # Update the status if necessary
-            if prev_state.status != current_state.status:
-                info('{} status changed from {} to {}'.format(name, prev_state.status, current_state.status))
-
-                msg = {'event': 'setImage', 'context': context, 'payload': {}}
-                if current_state.status == 'OFF':
-                    msg['payload']['image'] = off_images[index]
-                elif current_state.status == 'ON':
-                    msg['payload']['image'] = on_images[index]
-                elif current_state.status in ['NONE', 'UNKNOWN']:
-                    msg['payload']['image'] = none_images[index]
-                else:
-                    raise Exception('Unexpected status {}'.format(current_state.status))
-
-                await ws.send(json.dumps(msg))
-
-            # Update the error if necessary
-            if prev_state.error != current_state.error:
-                info('{} error changed from {} to {}'.format(name, prev_state.error, current_state.error))
-
-                await ws.send(
-                    json.dumps({
-                        'event': 'setTitle',
-                        'context': context,
-                        'payload': {'title': current_state.error}}))
 
 
 # Get the plugin version string from manifest.json
@@ -336,7 +176,7 @@ Command handler for an Elgato Stream Deck plugin for Facebook actions.
             an='StreamDeckWorkrooms', av=plugin_version, aip=1, npa=1)
 
         async_tasks += [streamdeck.listen(ws, analytics_collect, action_metadata)]
-        async_tasks += [browser_listen(ws, analytics_collect, on_images, off_images, unknown_images, none_images)]
+        async_tasks += [browser.listen(ws, analytics_collect, action_metadata, on_images, off_images, unknown_images, none_images)]
 
         await analytics_collect(t='event', ec='System', ea='Launch')
 
