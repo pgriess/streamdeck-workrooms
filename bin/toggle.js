1e717eec6adff367ad74f1dc3ead21785b36f195
Peter Griess <peter.griess@oculus.com>
2021-09-14 09:22:24 -0500

    Fix several issues related to inviteable rooms
    - The previous query/toggle JavaScript regexes didn't handle the "Join
      Room" case; they were always expecting "Join as X".
    - The logic for matching <span>s was using Node.textContent, which
      recursively accumulates text content from all children. In the case of
      nested <span> elements this means that each <span> will have the same
      text content. In query.js, this meant that we ended up with several
      matching nodes, which meant that our final regex didn't match "Join
      RoomJoin RoomJoin Room". Tighten this up by filtering for nodes that
      have no children (i.e. leaf nodes).
    
    Fixes #39

--- bin/toggle.js
+++ bin/toggle.js
@@ -5,7 +5,7 @@
     let micButtonRegex = /^(mute|unmute) microphone$/i;
     let cameraButtonRegex = /^turn (off|on) (video|camera)$/i;
     let handButtonRegex = /^(raise|lower) hand$/i;
-    let callButtonRegex = /^(join as )|(end call)/i;
+    let callButtonRegex = /^(join as )|(join room)|(end call)/i;
     // ***** END *****
 
     let re =
@@ -20,6 +20,10 @@
     }
 
     Array.from(document.querySelectorAll('span'))
+        // Node.textContent rolls up text from all child nodes. If there are
+        // nested spans, this will cause /all/ of the spans to match. Really we
+        // just want the leaf. Strip out all nodes that have children.
+        .filter((n) => { return n.childElementCount === 0 })
         .filter((n) => { return n.textContent.match(re); })
         .forEach((b) => { b.click(); });
 }
