9ee8ed694c7180afb1ae121a736d3277c8ac14c3
Mark Reitblatt <mark@reitblatt.com>
2022-12-14 16:18:16 -0800

    Fixed state detection and button toggling for web. Not working for Mac app, not sure if it ever did before.

--- bin/toggle.js
+++ bin/toggle.js
@@ -5,7 +5,7 @@
     let micButtonRegex = /^(mute|unmute) microphone$/i;
     let cameraButtonRegex = /^turn (off|on) (video|camera)$/i;
     let handButtonRegex = /^(raise|lower) hand$/i;
-    let callButtonRegex = /^(join as )|(join room)|(end call)/i;
+    let callButtonRegex = /^(join as )|(join room)|(join workroom)|(end call)/i;
     // ***** END *****
 
     let re =
@@ -19,11 +19,8 @@
         return;
     }
 
-    Array.from(document.querySelectorAll('span'))
-        // Node.textContent rolls up text from all child nodes. If there are
-        // nested spans, this will cause /all/ of the spans to match. Really we
-        // just want the leaf. Strip out all nodes that have children.
-        .filter((n) => { return n.childElementCount === 0 })
-        .filter((n) => { return n.textContent.match(re); })
+    Array.from(document.querySelectorAll('div'))        
+        .filter((n) => { return n.ariaLabel != null })
+        .filter((n) => { return n.ariaLabel.match(re); })
         .forEach((b) => { b.click(); });
 }
